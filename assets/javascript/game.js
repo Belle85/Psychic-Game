e5b5b37bea9f539d6fae831cf6d1b1137a20233f
Belle Chauviray <bebold.bebelle@gmail.com>
2018-05-27 18:04:08 -0400

    Wrote comments/instuctions for my JS file.

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+//Generate a random letter and store it in a variable.
+
+//Prompt user and ask to guess a letter from A to Z.
+
+//Check to see if letter is correct. If correct, add +1 to Wins
+
+/*If letter is wrong, prompt user again and ask to try to guess again. Repeat until letter is guessed or until user runs out of attempts.
+If letter not guess, then add +1 to losses.*/
+
+
+//If letter is correct then display the correct letter. Add 1 to Wins.
