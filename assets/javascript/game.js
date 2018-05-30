c6eb5d1fd48b1a0a20bee40a2d745c9de9c6820d
Belle Chauviray <bebold.bebelle@gmail.com>
2018-05-29 22:09:10 -0400

    Fixed code to display the letter guesses instead of undefined.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,51 @@
-// //Creates an array that lists out all of the options.
-// var randomLetter = ["a";"b";"c";"d";"e";"f";"g";"h";"i";"j";"k";"l";"m";"n";"o";"p";"q";"r";"s";"t";"u";"v";"w";"x";"y";"z"];
+//Creates an array that lists out all of the options.
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+//Create variable where the guessed letter will be pushed to.
+var letterGuessed =[];
+//Create variable to store the number of wins.
+var wins = 0;
+//Create variable to store the number of losses.
+var losses = 0;
+//Create variable to keep track of the guesses.
+var totalGuesses = 0;
+//Create variable to limit the number of tries.
+var maxTries = 15; 
+//Create variable for the computer random guesses.
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
-//  // This function is run whenever the user presses a key.
-//  document.onkeyup = function(event) {
+// This function is run whenever the user presses a key.
+document.onkeyup = function (event) {
 
-// // Determines which key was pressed.
-// var userGuess = event.key;
+    // Determines which key was pressed.
+     var userGuess = event.key;
 
-// // Randomly chooses a choice from the options array. This is the Computer's guess.
-// var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    // Logs in the console what the computer chose for control.
+    console.log("Computer guess:" + computerGuess);
 
-// //If letter is correct then display the correct letter. Add 1 to Wins.
+    //Pushes what the user guessed
+    letterGuessed.push(userGuess);
+   
+    //Logs in the console what the user guessed.
+    console.log(letterGuessed);
+
+    //If the user guesses wrong
+    if (userGuess != computerGuess) {
+        maxTries--;
+        if (maxTries === 0) {
+            losses++
+            document.getElementById("losses").innerHTML = losses;
+            alert("GAME OVER! Try again!");
+        }
+        totalGuesses++;
+        document.getElementById("guessesLeft").innerHTML = maxTries;
+        document.getElementById("guessed").innerHTML = letterGuessed;
+    }
+    if (userGuess == computerGuess) {
+        maxTries--;
+        totalGuesses++;
+        wins++;
+        document.getElementById("wins").innerHTML = wins;
+        alert("You won! Try again!");
+    }
+}
 
