aa583b933cca47602f080632ad9f635fdf65d693
Belle Chauviray <bebold.bebelle@gmail.com>
2018-05-30 09:29:35 -0400

    Commented my JS code.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,10 +6,8 @@ var letterGuessed =[];
 var wins = 0;
 //Create variable to store the number of losses.
 var losses = 0;
-//Create variable to keep track of the guesses.
-var totalGuesses = 0;
 //Create variable to limit the number of tries.
-var maxTries = 15; 
+var maxTries = 10; 
 //Create variable for the computer random guesses.
 var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
@@ -22,30 +20,32 @@ document.onkeyup = function (event) {
     // Logs in the console what the computer chose for control.
     console.log("Computer guess:" + computerGuess);
 
-    //Pushes what the user guessed
+    //Pushes what the user guessed.
     letterGuessed.push(userGuess);
    
     //Logs in the console what the user guessed.
     console.log(letterGuessed);
 
-    //If the user guesses wrong
+    //If the user guesses incorrect.
     if (userGuess != computerGuess) {
-        maxTries--;
-        if (maxTries === 0) {
-            losses++
-            document.getElementById("losses").innerHTML = losses;
-            alert("GAME OVER! Try again!");
+        maxTries--;//Reduces the number of tries by 1.
+        if (maxTries === 0) { //When there is not more try left.
+            losses++//Add one to the losses counter.
+            document.getElementById("losses").innerHTML = losses;//Displays the losses in the HTML document.
+            alert("GAME OVER! Try again!");//Pop up.
+            maxTries = 10;//Resets the number of tries to 10.
+            letterGuessed = [];//Clears the letter already guessed.
+            computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];//Computer makes another letter choice.
+           
         }
-        totalGuesses++;
-        document.getElementById("guessesLeft").innerHTML = maxTries;
-        document.getElementById("guessed").innerHTML = letterGuessed;
+        document.getElementById("guessesLeft").innerHTML = maxTries;//Displays the guesses left in the HTML document.
+        document.getElementById("guessed").innerHTML = letterGuessed;//Displays the letter guessed in the HTML document.
     }
-    if (userGuess == computerGuess) {
-        maxTries--;
-        totalGuesses++;
-        wins++;
-        document.getElementById("wins").innerHTML = wins;
-        alert("You won! Try again!");
+    if (userGuess == computerGuess) {//If the user guesses correct.
+        maxTries--;//Reduces the number of tries by 1.
+        wins++;//Increases the number of wins by 1.
+        document.getElementById("wins").innerHTML = wins;//Displays the wins in the HTML document.
+        alert("You won! Try again!");//Pop up.
     }
 }
 
